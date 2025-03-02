588c3fe191fe718f893d1b166fa3e678b3f202d0
Anantajit Subrahmanya <anantajit@ucsb.edu>
2025-03-02 00:17:23 +0000

    Added union operator to Sequential Reeb Graph

--- ReebGraph.py
+++ ReebGraph.py
@@ -1,6 +1,7 @@
 from networkx.classes import DiGraph
 from dataclasses import dataclass
 import numpy as np
+from collections.abc import Iterable
 
 """
 Sequential Reeb Graph - assumes no explicit time dimension (fixed sampling rate)
@@ -26,8 +27,9 @@ class SequentialReebGraph(DiGraph):
 
         self.bundle_centers = None 
         self.bundle_trajectories = None
+    
 
-    def append_trajectory(self, traj: np.ndarray):
+    def __append(self, traj: np.ndarray):
         # append a trajectory to the reeb graph
         if (self.store_trajectories and self.trajectories is None) or (not self.store_trajectories and self.trajectories == 0):
             self.bundle_centers = [np.array([traj[i]], ndmin=1) for i in range(traj.shape[0])]
@@ -56,8 +58,11 @@ class SequentialReebGraph(DiGraph):
             self.trajectories = np.vstack((self.trajectories, traj.reshape(1, -1)))
         else:
             self.trajectories += 1
-    
-    def build_graph(self):
+
+    def __build_graph(self):
+        # delete all nodes and edges
+        self.clear()
+
         # start by adding all the bundles at the start of the graph
         active_nodes = np.hstack((np.zeros((self.bundle_centers[0].shape[0], 1)), self.bundle_centers[0]))
         active_bundles = [tuple(bundle) for bundle in self.bundle_trajectories[0]]
@@ -106,3 +111,53 @@ class SequentialReebGraph(DiGraph):
             # add the new bundles
             active_bundles += new_bundles
             active_nodes = np.vstack((active_nodes, new_nodes))
+
+    def append_trajectory(self, traj: np.ndarray, compute_graph=True):
+        self.__append(traj)
+        if compute_graph:
+            self.__build_graph()
+        
+    def append_trajectories(self, trajs: Iterable, compute_graph=True): 
+        for traj in trajs:
+            self.__append(traj)
+        if compute_graph:
+            self.__build_graph()
+    
+    def union(self, other, compute_graph=True, adjust_epsilon=1):
+        assert self.dist == other.dist, "Distance functions must be the same"
+        assert self.epsilon == other.epsilon, "Epsilon must be the same" 
+        assert self.store_trajectories == other.store_trajectories, "Flag store_trajectories must be the same"
+
+        union = SequentialReebGraph(self.dist, self.epsilon, self.store_trajectories)
+
+        if union.store_trajectories:
+            assert self.trajectories.shape[1:] == other.trajectories.shape[1:], "All axes except sequential axis must match"
+
+        if self.store_trajectories and other.store_trajectories:
+            union.trajectories = np.vstack((self.trajectories, other.trajectories))
+        else:
+            union.trajectories = self.trajectories + other.trajectories 
+        
+        union.bundle_centers = self.bundle_centers
+        union.bundle_trajectories = self.bundle_trajectories
+
+        self_trajectory_count = self.trajectories.shape[0] if self.store_trajectories else self.trajectories
+        
+        # for each time step, find non-intersecting bundle centers
+        # give preference to self's bundle centers
+        for t in range(len(union.bundle_centers)):
+            for center, trajs in zip(other.bundle_centers[t], other.bundle_trajectories[t]):
+                # compute the distance between this center and all union bundle centers at this time
+                distances = union.dist(union.bundle_centers[t], center, axis=1)
+                if np.all(distances > union.epsilon * adjust_epsilon):
+                    union.bundle_centers[t] = np.vstack((union.bundle_centers[t], center))
+                    union.bundle_trajectories[t] += [np.array(trajs, ndmin=1) + self_trajectory_count]
+                else:
+                    # map this center to the nearest existing bundle
+                    nearest_bundle = np.argmin(distances)
+                    union.bundle_trajectories[t][nearest_bundle] = np.hstack((union.bundle_trajectories[t][nearest_bundle], np.array(trajs, ndmin=1) + self_trajectory_count))
+        
+        if compute_graph:
+            union.__build_graph()
+
+        return union
